59da393bb77512149a34c4f88ad5741854aeed03
lyona-lyo <118660631+lyona-lyo@users.noreply.github.com>
2023-03-26 23:59:10 +0300

    bugs fix

--- curiefense/curieproxy/rust/curiefense/src/interface/mod.rs
+++ curiefense/curieproxy/rust/curiefense/src/interface/mod.rs
@@ -7,7 +7,7 @@ use crate::logs::Logs;
 use crate::utils::json::NameValue;
 use crate::utils::templating::{parse_request_template, RequestTemplate, TVar, TemplatePart};
 use crate::utils::{selector, GeoIp, RequestInfo, Selected};
-use chrono::DurationRound::{duration_trunc};
+use chrono::DurationRound::duration_trunc;
 use serde::ser::{SerializeMap, SerializeSeq};
 use serde::{Deserialize, Serialize, Serializer};
 use std::collections::{HashMap, HashSet};
