bcbf9f117bd3c66a9d716a0ba4b3fb5fcbad4098
yoavkatzman <yoavk@reblaze.com>
2023-03-27 16:53:55 +0300

    initial bootstrap fix
    Signed-off-by: Curiefense Bootstrap Script <yoavk@reblaze.com>

--- curiefense/images/confserver/init/entrypoint.sh
+++ curiefense/images/confserver/init/entrypoint.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
-
 # make sure git trusts the persistennt directories.
+
 git config --global --add safe.directory /cf-persistent-config/confdb
 git config --global --add safe.directory /cf-persistent-config/bootstrap-repo
 
 if [ "$INIT_GIT_ON_STARTUP" = "yes" ]; then
 	# used when running with docker-compose, which does not have initContainers or similar features
-	/bootstrap/bootstrap_config.sh
+	for i in {1..3}
+  do
+	  /bootstrap/bootstrap_config.sh
+	  if [ $? -eq 0 ]; then
+	    echo "error - entrypoint.sh bootstrap_config failed, attempt number $i, trying again"
+      break
+    fi
+  done
 fi
 
 /bootstrap/initial-bucket-export.sh &
