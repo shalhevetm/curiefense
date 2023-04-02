799ef8319db5675ea81e7bfdfbfccbf612e5517f
yoavkatzman <yoavk@reblaze.com>
2023-04-02 12:38:45 +0300

    indentation
    Signed-off-by: Curiefense Bootstrap Script <yoavk@reblaze.com>

--- curiefense/images/confserver/init/entrypoint.sh
+++ curiefense/images/confserver/init/entrypoint.sh
@@ -5,11 +5,10 @@ git config --global --add safe.directory /cf-persistent-config/confdb
 git config --global --add safe.directory /cf-persistent-config/bootstrap-repo
 
 if [ "$INIT_GIT_ON_STARTUP" = "yes" ]; then
-	# used when running with docker-compose, which does not have initContainers or similar features
-	# due to unexpected failures, bootstrap will run up to 3 times to better eliminate these failures (usually fixed after one failure)
-	for i in {1..3}
-  do
-	  if /bootstrap/bootstrap_config.sh; then
+  # used when running with docker-compose, which does not have initContainers or similar features
+  # due to unexpected failures, bootstrap will run up to 3 times to better eliminate these failures (usually fixed after one failure)
+  for i in {1..3}; do
+    if /bootstrap/bootstrap_config.sh; then
       break
     fi
     echo "error - entrypoint.sh bootstrap_config failed, attempt number $i, trying again"
